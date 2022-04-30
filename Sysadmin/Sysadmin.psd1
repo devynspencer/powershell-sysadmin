e522f61a6ed1afeaa2e49bfc195c6180061bc2d3
Devyn Spencer <devynspencer@users.noreply.github.com>
2022-04-29 17:29:02 -0700

    Add Uninstall-Application function

--- Sysadmin/Sysadmin.psd1
+++ Sysadmin/Sysadmin.psd1
@@ -68,6 +68,7 @@
         'New-Shortcut'
         'Set-PrintShortcutShareContent'
         'Restore-HostsFile'
+        'Uninstall-Application'
     )
 
     # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
---
+++ Sysadmin/public/Uninstall-Application.ps1
@@ -0,0 +1,50 @@
+function Uninstall-Application {
+    [CmdletBinding(DefaultParameterSetName = 'ByName')]
+    param (
+        # Application name to uninstall, wildcards encouraged
+        [Parameter(Mandatory, ParameterSetName = 'ByName')]
+        [string[]]
+        $Name,
+
+        [Parameter(Mandatory)]
+        [ValidateSet('32-bit', '64-bit')]
+        [string[]]
+        $Architecture
+    )
+
+    $InstallData = switch ($Architecture) {
+        '64-bit' {
+            Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*'
+        }
+
+        '32-bit' {
+            Get-ItemProperty 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*'
+        }
+    }
+
+    $Applications = switch ($PSCmdlet.ParameterSetName) {
+        'ByName' {
+            foreach ($AppName in $Name) {
+                $InstallData.where({ $_.DisplayName -like "$AppName" })
+            }
+        }
+    }
+
+    Write-Verbose "Found [$($Applications.Count)] applications matching query"
+
+    foreach ($App in $Applications) {
+        Write-Verbose ('Uninstalling software [{0}] version [1]' -f $App.DisplayName, $App.DisplayVersion)
+
+        $UninstallParams = @{
+            FilePath = 'msiexec.exe'
+            NoNewWindow = $true
+            Wait = $true
+            ArgumentList = @(
+                '/x'
+                $App.PSChildName
+            )
+        }
+
+        Start-Process @UninstallParams
+    }
+}
