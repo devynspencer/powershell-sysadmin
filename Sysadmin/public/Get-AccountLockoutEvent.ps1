9e4ad1c624c638c34b29a96231fc47d6e17e03e8
Devyn Spencer <devynspencer@users.noreply.github.com>
2022-08-17 00:25:50 -0700

    Refactor Get-AccountLockoutEvent
    Update to match Get-LogonFailureEvent and other Windows event log functions.

--- Sysadmin/public/Get-AccountLockoutEvent.ps1
+++ Sysadmin/public/Get-AccountLockoutEvent.ps1
@@ -1,39 +1,47 @@
-<#
-    .SYNOPSIS
-        Get account lockout events from the PDC emulator.
-
-    .PARAMETER Days
-        Number of days of event logs to include.
-
-    .EXAMPLE
-        Get-AccountLockoutEvent
-
-    .EXAMPLE
-        Get-AccountLockoutEvent -Days 7
-#>
-
 function Get-AccountLockoutEvent {
-    [CmdletBinding()]
-    param (
-        $Days = 3
+    Param (
+        [Microsoft.ActiveDirectory.Management.ADUser]
+        $Identity,
+
+        [string]
+        $Server = (Get-ADDomain).PDCEmulator,
+
+        [datetime]
+        $StartTime = (Get-Date).AddDays(-3),
+
+        [datetime]
+        $EndTime
     )
 
-    $FilterParameters = @{
-        LogName = 'Security'
-        Id = 4740
-        StartTime = (Get-Date).AddDays(-$Days)
+    begin {
+        $EventFilter = @{
+            LogName = 'Security'
+            Id = 4740
+        }
+
+        if ($PSBoundParameters.ContainsKey('StartTime')) {
+            $EventFilter.StartTime = $StartTime
+        }
+
+        if ($PSBoundParameters.ContainsKey('EndTime')) {
+            $EventFilter.EndTime = $EndTime
+        }
+
+        $Events = Get-WinEvent -ComputerName $Server -FilterHashtable $EventFilter
     }
 
-    $PDCEmulator = (Get-ADDomain).PDCEmulator
-    $LockoutEvents = Get-WinEvent -ComputerName $PDCEmulator -FilterHashtable $FilterParameters
+    process {
+        if ($PSBoundParameters.ContainsKey('Identity')) {
+            $User = Get-ADUser $Identity
+            $Events = $Events | ? { $_.Properties[0].Value -eq $User.SamAccountName }
+        }
 
-    foreach ($Event in $LockoutEvents) {
-        [pscustomobject] @{
-            AccountName = $Event.properties[0].value
-            CallerName = $Event.properties[1].value
-            AccountSid = $Event.properties[2].value.value
-            DomainController = $Event.properties[4].value
-            Domain = $Event.properties[5].value
+        foreach ($Event in $Events) {
+            [pscustomobject] @{
+                TargetAccount = $Event.properties.Value[0]
+                CallingComputer = $Event.Properties.Value[1]
+                TimeCreated = $Event.TimeCreated
+            }
         }
     }
 }
