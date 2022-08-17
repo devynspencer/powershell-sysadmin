ec5ce0af5db413a5a8504a6235fa9245edcadb23
Devyn Spencer <devynspencer@users.noreply.github.com>
2022-08-17 00:22:46 -0700

    Add Get-LogonFailureEvent function

--- Sysadmin/Sysadmin.psd1
+++ Sysadmin/Sysadmin.psd1
@@ -70,6 +70,7 @@
         'Set-PrintShortcutShareContent'
         'Restore-HostsFile'
         'Uninstall-Application'
+        'Get-LogonFailureEvent'
     )
 
     # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
---
+++ Sysadmin/public/Get-LogonFailureEvent.ps1
@@ -0,0 +1,61 @@
+function Get-LogonFailureEvent {
+    Param (
+        [Microsoft.ActiveDirectory.Management.ADUser]
+        $Identity,
+
+        [string]
+        $Server = (Get-ADDomain).PDCEmulator,
+
+        [datetime]
+        $StartTime = (Get-Date).AddDays(-3),
+
+        [datetime]
+        $EndTime
+    )
+
+    begin {
+        $LogonType = @{
+            '2' = 'Interactive'
+            '3' = 'Network'
+            '4' = 'Batch'
+            '5' = 'Service'
+            '7' = 'Unlock'
+            '8' = 'Networkcleartext'
+            '9' = 'NewCredentials'
+            '10' = 'RemoteInteractive'
+            '11' = 'CachedInteractive'
+        }
+
+        $EventFilter = @{
+            LogName = 'Security'
+            Id = 4625
+        }
+
+        if ($PSBoundParameters.ContainsKey('StartTime')) {
+            $EventFilter.StartTime = $StartTime
+        }
+
+        if ($PSBoundParameters.ContainsKey('EndTime')) {
+            $EventFilter.EndTime = $EndTime
+        }
+
+        $Events = Get-WinEvent -ComputerName $Server -FilterHashtable $EventFilter
+    }
+
+    process {
+        if ($PSBoundParameters.ContainsKey('Identity')) {
+            $User = Get-ADUser $Identity
+            $Events = $Events | ? { $_.Properties[5].Value -eq $User.SamAccountName }
+        }
+
+        foreach ($Event in $Events) {
+            [pscustomobject] @{
+                TargetAccount = $Event.properties.Value[5]
+                LogonType = $LogonType."$($Event.properties.Value[10])"
+                CallingComputer = $Event.Properties.Value[13]
+                IPAddress = $Event.Properties.Value[19]
+                TimeCreated = $Event.TimeCreated
+            }
+        }
+    }
+}
