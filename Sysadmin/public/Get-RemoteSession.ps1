bddc8b89557ecce887e47874517f57bc2c1df85e
Devyn Spencer <devynspencer@users.noreply.github.com>
2022-08-17 11:24:36 -0700

    Refactor Get-RemoteSession to support filtering
    Any number of parameters can be added, checking a specific criteria.
    
    If the criteria is unmet the $Include flag can be set to $false and the session object won't be included in function output.

--- Sysadmin/public/Get-RemoteSession.ps1
+++ Sysadmin/public/Get-RemoteSession.ps1
@@ -39,21 +39,30 @@ function Get-RemoteSession {
 
             # Extract session data from each line of output
             foreach ($Line in $Output) {
-                $HeaderFields = 'SessionName', 'UserName', 'Id', 'State', 'Type', 'Device'
+                # Whether to output the object after any filtering. Using a flag like this should be
+                # more performant (and slightly more idiomatic) than adding all sessions to an array
+                # outside of the loop and *then* filtering
+                $Include = $true
 
                 # Convert fixed-width fields into CSV format. Note the leading space (thus 2 spaces
                 # minimum) and that each line can potentially have *multiple* blank columns (thus 19
                 # characters maximum)
-                $Session = $Line -replace '\s{2,19}', ',' | ConvertFrom-Csv -Header $HeaderFields
+                $Session = $Line -replace '\s{2,19}', ',' | ConvertFrom-Csv -Header 'SessionName', 'UserName', 'Id', 'State', 'Type', 'Device'
 
-                [pscustomobject] @{
-                    ComputerName = $Computer
-                    SessionName = $Session.SessionName
-                    UserName = $Session.UserName
-                    SessionId = $Session.Id
-                    State = $Session.State
-                    Type = $Session.Type
-                    Device = $Session.Device
+                # Convert state value from shorthand to expanded/pretty value
+                $ExpandedState = $SessionStates.GetEnumerator().where({ $_.Value -eq $Session.State }).Name
+
+                if ($Include) {
+                    # Build the output object
+                    [pscustomobject] @{
+                        ComputerName = $Computer
+                        SessionName = $Session.SessionName
+                        UserName = $Session.UserName
+                        SessionId = $Session.Id
+                        State = $ExpandedState
+                        Type = $Session.Type
+                        Device = $Session.Device
+                    }
                 }
             }
         }
