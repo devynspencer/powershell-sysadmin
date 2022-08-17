84360899e58d69a19bc5777bddf2e062e9750bcc
Devyn Spencer <devynspencer@users.noreply.github.com>
2022-08-17 00:28:20 -0700

    Add Get-RemoteSession function

--- Sysadmin/Sysadmin.psd1
+++ Sysadmin/Sysadmin.psd1
@@ -72,6 +72,7 @@
         'Uninstall-Application'
         'Get-LogonFailureEvent'
         'Get-AccountLockoutEvent'
+        'Get-RemoteSession'
     )
 
     # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
---
+++ Sysadmin/public/Get-RemoteSession.ps1
@@ -0,0 +1,43 @@
+function Get-RemoteSession {
+    param (
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [string[]]
+        $ComputerName
+    )
+
+    begin {
+        # Skip unreachable hosts
+        $ReachableHosts = $ComputerName | ? { Test-Connection $_ -Quiet -Count 1 }
+    }
+
+    process {
+        foreach ($Computer in $ReachableHosts) {
+            # Query for remote sessions
+
+            # TODO: Start-Process punts the output for some reason
+            # https://stackoverflow.com/questions/8761888/capturing-standard-out-and-error-with-start-process
+
+            # Skip the first (header) line
+            $Output = qwinsta /server:$Computer | select -Skip 1
+
+            foreach ($Line in $Output) {
+                $HeaderFields = 'SessionName', 'UserName', 'Id', 'State', 'Type', 'Device'
+
+                # Convert fixed-width fields into CSV format. Note the leading space (thus 2 spaces
+                # minimum) and that each line can potentially have *multiple* blank columns (thus 19
+                # characters maximum)
+                $Session = $Line -replace '\s{2,19}', ',' | ConvertFrom-Csv -Header $HeaderFields
+
+                [pscustomobject] @{
+                    ComputerName = $Computer
+                    SessionName = $Session.SessionName
+                    UserName = $Session.UserName
+                    Id = $Session.Id
+                    State = $Session.State
+                    Type = $Session.Type
+                    Device = $Session.Device
+                }
+            }
+        }
+    }
+}
