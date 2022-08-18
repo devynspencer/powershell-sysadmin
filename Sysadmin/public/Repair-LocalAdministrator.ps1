8bb7b69d7d3112baf03d6194983062fe948c9bc0
Devyn Spencer <devynspencer@users.noreply.github.com>
2022-08-17 20:59:11 -0700

    Add Repair-LocalAdministrator function
    Because periodically discovering the local Administrator account has been disabled is bad for my mental health.

---
+++ Sysadmin/public/Repair-LocalAdministrator.ps1
@@ -0,0 +1,43 @@
+function Repair-LocalAdministrator {
+    param (
+        # Host(s) to operate on
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [string[]]
+        $ComputerName
+    )
+
+    process {
+        foreach ($Computer in $ComputerName) {
+            $InvokeParams = @{
+                ComputerName = $Computer
+                HideComputername = $true
+                ErrorAction = 'SilentlyContinue'
+            }
+
+            Invoke-Command @InvokeParams {
+                # Get-LocalUser doesn't want to output a useful set of properties by default
+                $Properties = 'Name', 'Enabled', 'AccountExpires', 'PasswordExpires'
+
+                # Using the Microsoft.PowerShell.LocalAccounts module instead of net user or similar
+                $LocalAdminParams = @{
+                    Name = 'Administrator'
+                }
+
+                $OriginalState = Get-LocalUser @LocalAdminParams | select $Properties
+
+                # Maintain idempotency, only update account when necessary
+                if (!$OriginalState.Enabled) {
+                    Enable-LocalUser @LocalAdminParams -WhatIf
+                }
+
+                if ($OriginalState.AccountExpires) {
+                    Set-LocalUser @LocalAdminParams -AccountNeverExpires -WhatIf
+                }
+
+                if ($OriginalState.PasswordExpires) {
+                    Set-LocalUser @LocalAdminParams -PasswordNeverExpires -WhatIf
+                }
+            }
+        }
+    }
+}
