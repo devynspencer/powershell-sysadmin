d741f4638ba74a9b686a729288be8ad994908eed
devynspencer <devynspencer@users.noreply.github.com>
2023-05-17 16:27:20 -0700

    Add Get-OrgFileShare function

---
+++ Sysadmin/public/Get-OrgFileShare.ps1
@@ -0,0 +1,45 @@
+function Get-OrgFileShare {
+    param (
+        # TODO: Support file objects as well...
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName, ParameterSetName = 'ByPath')]
+        [string[]]
+        $Path
+    )
+
+    process {
+        foreach ($SharePath in $Path) {
+            # Resolve share, site, and group names
+            $ShareName = $SharePath | Split-Path -Leaf
+            $SiteName = $SharePath | Split-Path -Parent | Split-Path -Leaf
+            $GroupName = "FS - $SiteName $ShareName"
+
+            # Search share permissions for corresponding share group (based on naming standard)
+            $Acl = Get-Acl -Path $Path
+
+            foreach ($Entry in $Acl.Access.IdentityReference) {
+                $CurrentIdentity = ($Entry -split '\\')[-1]
+                Write-Verbose "Current ACL entry: $Entry ($CurrentIdentity)"
+
+                if ($CurrentIdentity -eq $GroupName) {
+                    Write-Verbose "Matching share group found! [$GroupName]`n"
+                    $Group = $CurrentIdentity
+                    break
+                }
+            }
+
+            # Return the share object
+            [pscustomobject] @{
+                Name = $ShareName
+                Site = $SiteName
+                Path = $SharePath
+                GroupName = $GroupName
+                HasGroup = [bool] $Group
+                Owner = $Acl.Owner
+                Access = $Acl.Access
+            }
+
+            # Cleanup found group
+            $Group = $null
+        }
+    }
+}
