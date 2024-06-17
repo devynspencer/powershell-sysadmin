995cac875b06975f158d117ad9a8e2c415209d2c
Devyn Spencer <devynspencer@users.noreply.github.com>
2024-06-17 12:26:01 -0700

    Update Resolve-OrgFileShareGroup.ps1
    Add support for pipeline input.

--- Sysadmin/public/Resolve-OrgFileShareGroup.ps1
+++ Sysadmin/public/Resolve-OrgFileShareGroup.ps1
@@ -1,16 +1,18 @@
 function Resolve-OrgFileShareGroup {
     param (
-        [Parameter(Mandatory)]
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
         [ValidateScript({ Test-Path -Path $_ })]
         [string[]]
         $Path
     )
 
-    foreach ($FileSharePath in $Path) {
-        # Get the directory name and parent directory name
-        $DirectoryName = (Split-Path -Path $FileSharePath -Leaf)
-        $ParentDirectoryName = (Split-Path -Path (Split-Path -Path $FileSharePath -Parent) -Leaf)
+    process {
+        foreach ($FileSharePath in $Path) {
+            # Get the directory name and parent directory name
+            $DirectoryName = (Split-Path -Path $FileSharePath -Leaf)
+            $ParentDirectoryName = (Split-Path -Path (Split-Path -Path $FileSharePath -Parent) -Leaf)
 
-        return "FS - $ParentDirectoryName $DirectoryName"
+            return "FS - $ParentDirectoryName $DirectoryName"
+        }
     }
 }
