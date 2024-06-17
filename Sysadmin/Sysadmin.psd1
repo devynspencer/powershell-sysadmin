8bd1eb68fe4dff31d405db436be1ab278870fd9a
Devyn Spencer <devynspencer@users.noreply.github.com>
2024-06-17 11:16:51 -0700

    Add function Test-OrgUserDirectoryAcl

--- Sysadmin/Sysadmin.psd1
+++ Sysadmin/Sysadmin.psd1
@@ -87,6 +87,7 @@
         'New-OrgFileShareGroup'
         'Clear-AclOrphanEntry'
         'Repair-OrgUserDirectoryAcl'
+        'Test-OrgUserDirectoryAcl'
     )
 
     # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
---
+++ Sysadmin/public/Test-OrgUserDirectoryAcl.ps1
@@ -0,0 +1,51 @@
+function Test-OrgUserDirectoryAcl {
+    param (
+        # Path to the user directory to test
+        [Parameter(Mandatory)]
+        [string]
+        $Path,
+
+        # SamAccountName of the user to test
+        [Parameter(Mandatory)]
+        [string]
+        $SamAccountName
+    )
+
+    # Get the ACL for the directory
+    $Acl = Get-Acl -Path $Path
+
+    # Define the expected ACL entries
+    $ExpectedAclEntries = @(
+        @{ IdentityReference = 'BUILTIN\Administrators'; FileSystemRights = 'FullControl' },
+        @{ IdentityReference = 'NT AUTHORITY\SYSTEM'; FileSystemRights = 'FullControl' },
+        @{ IdentityReference = $SamAccountName; FileSystemRights = 'Modify' }
+    )
+
+    # Get the actual ACL entries
+    $ActualAclEntries = $Acl.Access | ForEach-Object {
+        @{ IdentityReference = $_.IdentityReference.Value; FileSystemRights = $_.FileSystemRights }
+    }
+
+    # Compare ACL entries against expected entries. For performance, only the number of entries is
+    # compared initially. If counts match, then each expected entry is compared against actual entries
+    $IsExpectedCount = $ExpectedAclEntries.Count -eq $ActualAclEntries.Count
+
+    if ($IsExpectedCount) {
+        foreach ($ExpectedEntry in $ExpectedAclEntries) {
+            # Compare each expected entry against actual entries
+            $MatchingActualEntry = $ActualAclEntries | where {
+                $_.IdentityReference -eq $ExpectedEntry.IdentityReference -and
+                $_.FileSystemRights -eq $ExpectedEntry.FileSystemRights
+            }
+
+            # If an expected entry is not found, set flag and break loop
+            if ($null -eq $MatchingActualEntry) {
+                $IsExpectedCount = $false
+                break
+            }
+        }
+    }
+
+    # Return whether all expected entries (and no others) were found
+    return $IsExpectedCount
+}
